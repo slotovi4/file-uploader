d7c32b095cdaa171ef0ee4ce03f3895b64784dce
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-11 15:34:37 +0300

    void

--- client/src/components/FileUpload/FileUpload.tsx
+++ client/src/components/FileUpload/FileUpload.tsx
@@ -4,35 +4,28 @@ import Message from '../Message/Message';
 import Progress from '../Progress/Progress';
 
 const FileUpload = () => {
-  const [file, setFile] = React.useState();
-  const [fileName, setFileName] = React.useState('Choose File');
+  const [file, setFile] = React.useState('');
+  const [filename, setFilename] = React.useState('Choose File');
   const [uploadedFile, setUploadedFile] = React.useState();
   const [message, setMessage] = React.useState('');
   const [uploadPercentage, setUploadPercentage] = React.useState(0);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const upFile = e.target.files ? e.target.files[0] : null;
-
-    if (upFile) {
-      const upName = upFile.name;
-
-      setFile(upFile);
-      setFileName(upName);
-    }
+  const onChange = (e: any) => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
   };
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: any) => {
     e.preventDefault();
-
     const formData = new FormData();
     formData.append('file', file);
 
     try {
       const res = await axios.post('/upload', formData, {
         headers: {
-          'Contant-Type': 'multipart/form-data'
+          'Content-Type': 'multipart/form-data'
         },
-        onDownloadProgress: ({ loaded, total }) => {
+        onUploadProgress: ({ loaded, total }) => {
           setUploadPercentage(Math.round((loaded * 100) / total));
 
           // Clear percentage
@@ -40,7 +33,10 @@ const FileUpload = () => {
         }
       });
 
-      setUploadedFile({ ...res.data });
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+
       setMessage('File Uploaded');
     } catch (err) {
       if (err.response.status === 500) {
@@ -57,34 +53,27 @@ const FileUpload = () => {
       <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
           <input
-            className="custom-file-input"
             type="file"
+            className="custom-file-input"
             id="customFile"
             onChange={onChange}
           />
           <label className="custom-file-label" htmlFor="customFile">
-            {fileName}
+            {filename}
           </label>
         </div>
 
         <Progress percentage={uploadPercentage} />
 
-        <input
-          className="btn btn-primary btn-block mt-4"
-          type="submit"
-          value="Upload"
-        />
+        <button className="btn btn-primary btn-block mt-4" type="submit">
+          Upload
+        </button>
       </form>
-
       {uploadedFile ? (
         <div className="row mt-5">
           <div className="col-md-6 m-auto">
             <h3 className="text-center">{uploadedFile.fileName}</h3>
-            <img
-              style={{ width: '100%' }}
-              src={uploadedFile.filePath}
-              alt={uploadedFile.filePath}
-            />
+            <img style={{ width: '100%' }} src={uploadedFile.filePath} alt="" />
           </div>
         </div>
       ) : null}
